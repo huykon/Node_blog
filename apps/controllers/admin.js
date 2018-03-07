afc8736ff1b115c5ee7dbde826710c1d7351b944
huykon225 <huykon225@gmail.com>
2018-03-07 11:40:51 +0700

    -up

---
+++ apps/controllers/admin.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var router = express.Router();
+
+var user_md = require("../models/user");
+
+
+router.get("/", function(req, res){
+	//res.json({"message": "This is admin page"});
+	res.render("admin/dashboard", {data: {error: false}});
+});
+
+router.get("/signup", function(req, res){
+	res.render("signup", {data: {}});
+});
+
+router.post("/signup", function(req, res){
+	var user = req.body;
+	if(user.email.trim().length == 0){
+		res.render("signup", {data: {error: "Email is required"}});
+	}
+
+	if(user.passwd.trim().length == 0){
+		res.render("signup", {data: {error: "Password is required"}});
+	}
+
+	if (user.passwd != user.repasswd && user.passwd.trim().length != 0) {
+		res.render("signup", {data: {error: "Password is not match"}});
+	}
+
+	// Insert to db
+	user = {
+		email: user.email,
+		password: user.passwd,
+		first_name: user.firstname,
+		last_name: user.lastname
+	};
+
+	var result = user_md.addUser(user);
+	if (!result) {
+		res.render("signup", {data: {error: "Could not insert user data to DB"}});
+	}else{
+		res.json({message: "Insert success"});
+	}
+
+});
+
+module.exports = router;
